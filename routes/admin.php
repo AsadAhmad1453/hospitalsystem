5dc3203489275e9b304e1031d60b377099605b7d
Muhammad Asad <94599676+AsadAhmad1453@users.noreply.github.com>
2025-08-05 15:14:46 +0500

    updated code

---
+++ routes/admin.php
@@ -0,0 +1,137 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\Admin\{
+    AdminDashboardController,
+    StaffController,
+    UsersController,
+    PatientController,
+    PermissionController,
+    ServiceController,
+    QuestionsController,
+    RoleController,
+    RelationController,
+    BankController,
+    FormController,
+    MedicController,
+    DoseController,
+    BloodInvController,
+    XrayController,
+    UltrasoundController,
+    CtscanController
+};
+  
+
+// Place all your admin routes here.
+
+Route::middleware(['auth', 'is_admin'])->prefix('admin')->group(function () {
+
+    Route::controller(AdminDashboardController::class)->group(function () {
+        Route::get('/', 'index')->name('admin-dashboard');
+        Route::get('/manage-profile', 'profile')->name('manage-profile');
+        Route::post('/update-profile', 'updateProfile')->name('update-profile');
+    });
+    
+    Route::controller(StaffController::class)->group(function () {
+        Route::post('/roles/bulk-update-permissions', 'bulkUpdatePermissions')->name('roles.bulkUpdatePermissions');
+        Route::get('/staff', 'index')->name('staff');
+        Route::post('/save-staff', 'savestaff')->name('save-staff');
+    });
+
+    Route::controller(UsersController::class)->group(function () {
+        Route::get('/users', 'index')->name('users');
+        Route::post('/save-user', 'saveuser')->name('save-user');
+        Route::get('/del-user/{id}', 'deluser')->name('del-user');
+        Route::get('/roles-table/{id}', 'rolestable')->name('roles-table');
+    });
+
+    Route::controller(PatientController::class)->group(function () {
+        Route::get('/patients', 'index')->name('patients');
+        Route::get('/patient-info/{id}', 'patientInfo')->name('patient-info');
+        Route::get('/del-patient/{id}', 'delPatient')->name('del-patient');
+        Route::get('/del-all', 'delAll')->name('del-all');
+    });
+
+    Route::controller(QuestionsController::class)->group(function () {
+        Route::get('/question-sections', 'index')->name('question-sections');
+        Route::post('/save-section', 'saveSection')->name('save-section');
+        Route::get('/delete-section/{id}', 'deleteSection')->name('del-section');
+        Route::get('/questions', 'question')->name('questions');
+        Route::get('/question-add', 'addQuestion')->name('question-add');
+        Route::post('/save-question', 'saveQuestion')->name('save-question');
+        Route::get('/delete-question/{id}', 'deleteQuestion')->name('del-question');
+        Route::post('/update-question-order', 'updateQuestionOrder')->name('update-question-order');
+        Route::get('/del-all-questions', 'delAllQuestions')->name('del-all-questions');
+    });
+   
+    Route::controller(PermissionController::class)->group(function () {
+        Route::get('/permissions', 'index')->name('permissions');
+        Route::post('/permissions', 'store')->name('save-permission');
+        Route::get('/permissions/{permission}', 'destroy')->name('del-perm');
+    });
+ 
+    Route::controller(ServiceController::class)->group(function () {
+        Route::get('/services', 'index')->name('services');
+        Route::post('/save-service', 'saveService')->name('save-service');
+        Route::get('/delete-service/{id}', 'deleteService')->name('del-service');
+    });
+    
+    Route::controller(RelationController::class)->group(function () {
+        Route::get('/question-relations', 'index')->name('relations');
+        Route::post('/save-question-relations', 'saveQuestionRelations')->name('save-question-relations');
+    });
+
+    Route::controller(BankController::class)->group(function () {
+        Route::get('/banks', 'index')->name('banks');
+        Route::post('/save-bank', 'saveBank')->name('save-bank');
+        Route::get('/delete-bank/{id}', 'deleteBank')->name('del-bank');
+    });
+
+    Route::controller(RoleController::class)->group(function () {
+        Route::get('/roles', 'index')->name('roles');
+        Route::post('/save-role', 'saveRole')->name('save-role');
+        Route::get('/delete-role/{id}', 'deleteRole')->name('del-role');
+    });
+
+    Route::controller(FormController::class)->group(function () {
+        Route::get('/forms', 'index')->name('forms');
+        Route::post('/save-form', 'saveForm')->name('save-form');
+        Route::get('/delete-form/{id}', 'deleteForm')->name('del-form');
+    });
+    
+    Route::controller(MedicController::class)->group(function () {
+        Route::get('/medicines', 'index')->name('medicines');
+        Route::post('/save-medic', 'saveMedic')->name('save-medic');
+        Route::get('/delete-medic/{id}', 'deleteMedic')->name('del-medic');
+    });
+   
+    Route::controller(DoseController::class)->group(function () {
+        Route::get('/dosage', 'index')->name('dosage');
+        Route::post('/save-dose', 'saveDose')->name('save-dose');
+        Route::get('/delete-dose/{id}', 'deleteDose')->name('del-dose');
+    });
+  
+    Route::controller(BloodInvController::class)->group(function () {
+        Route::get('/blood-investigation', 'index')->name('blood-investigation');
+        Route::post('/save-blood-inv', 'saveBloodInv')->name('save-blood-inv');
+        Route::get('/delete-blood-inv/{id}', 'deleteBloodInv')->name('del-blood-inv');
+    });
+   
+    Route::controller(XrayController::class)->group(function () {
+        Route::get('/x-rays', 'index')->name('xrays');
+        Route::post('/save-xray', 'saveXray')->name('save-xray');
+        Route::get('/delete-xray/{id}', 'deleteXray')->name('del-xray');
+    });
+    
+    Route::controller(UltrasoundController::class)->group(function () {
+        Route::get('/ultrasounds', 'index')->name('uss');
+        Route::post('/save-ultrasound', 'saveUltrasound')->name('save-us');
+        Route::get('/delete-ultrasound/{id}', 'deleteUltrasound')->name('del-us');
+    });
+    Route::controller(CtscanController::class)->group(function () {
+        Route::get('/ct-scans', 'index')->name('ctscans');
+        Route::post('/save-ctscan', 'save')->name('save-ctscan');
+        Route::get('/delete-ctscan/{id}', 'delete')->name('del-ctscan');
+    });
+    
+});
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\Admin\AdminDashboardController;
-use App\Http\Controllers\Admin\StaffController;
-use App\Http\Controllers\Admin\PermissionController;
-use App\Http\Controllers\Admin\UsersController;
-use App\Http\Controllers\Admin\PatientController;
+
 use App\Http\Controllers\User\UserController;
 use App\Http\Controllers\Customer\CustomerDashboardController;
 use App\Http\Controllers\User\WeatherController;
-use App\Http\Controllers\Admin\ServiceController;
 use App\Http\Controllers\User\PatientEntryController;
-use App\Http\Controllers\Admin\QuestionsController;
 use App\Http\Controllers\User\DataCollectorController;
 use App\Http\Controllers\User\DoctorController;
 use App\Http\Controllers\User\AIChatController;
@@ -30,81 +24,6 @@ use Illuminate\Support\Facades\Artisan;
 
 
 Auth::routes();
-Route::prefix('admin')->middleware(['auth','is_admin'])->group(function () {
-    Route::post('/roles/bulk-update-permissions', [StaffController::class, 'bulkUpdatePermissions'])->name('roles.bulkUpdatePermissions');
-    Route::get('/', [AdminDashboardController::class, 'index'])->name('admin-dashboard');
-    Route::get('/manage-profile', [AdminDashboardController::class, 'profile'])->name('manage-profile');
-    Route::post('/update-profile', [AdminDashboardController::class, 'updateProfile'])->name('update-profile');
-    Route::get('/users', [UsersController::class, 'index'])->name('users');
-    Route::get('/staff', [StaffController::class, 'index'])->name('staff');
-    Route::get('/patients', [PatientController::class, 'index'])->name('patients');
-    Route::get('/patient-info/{id}', [App\Http\Controllers\Admin\PatientController::class, 'patientInfo'])->name('patient-info');
-    Route::get('/question-sections', [QuestionsController::class, 'index'])->name('question-sections');
-    Route::post('/save-section', [QuestionsController::class, 'saveSection'])->name('save-section');
-    Route::get('/delete-section/{id}', [QuestionsController::class, 'deleteSection'])->name('del-section');
-
-    Route::get('/questions', [QuestionsController::class, 'question'])->name('questions');
-    Route::get('/question-add', [QuestionsController::class, 'addQuestion'])->name('question-add');
-    Route::post('/save-question', [QuestionsController::class, 'saveQuestion'])->name('save-question');
-    Route::get('/delete-question/{id}', [QuestionsController::class, 'deleteQuestion'])->name('del-question');
-    Route::post('/update-question-order', [QuestionsController::class, 'updateQuestionOrder'])->name('update-question-order');
-    Route::get('/del-all-questions', [QuestionsController::class, 'delAllQuestions'])->name('del-all-questions');
-
-
-    Route::get('/permissions', [PermissionController::class, 'index'])->name('permissions');
-    Route::post('/permissions', [PermissionController::class, 'store'])->name('save-permission');
-    Route::get('/permissions/{permission}', [PermissionController::class, 'destroy'])->name('del-perm');
-    Route::post('/save-staff', [StaffController::class, 'savestaff'])->name('save-staff');
-    Route::post('/save-user', [UsersController::class, 'saveuser'])->name('save-user');
-    Route::get('/del-user/{id}', [UsersController::class, 'deluser'])->name('del-user');
-    Route::get('/del-patient/{id}', [PatientController::class, 'delPatient'])->name('del-patient');
-    Route::get('/del-all', [PatientController::class, 'delAll'])->name('del-all');
-
-    Route::get('/roles-table/{id}', [UsersController::class, 'rolestable'])->name('roles-table');
-    Route::get('/services', [ServiceController::class, 'index'])->name('services');
-    Route::post('/save-service', [ServiceController::class, 'saveService'])->name('save-service');
-    Route::get('/delete-service/{id}', [ServiceController::class, 'deleteService'])->name('del-service');
-
-    Route::get('/question-relations', [App\Http\Controllers\Admin\RelationController::class, 'index'])->name('relations');
-    Route::post('/save-question-relations', [App\Http\Controllers\Admin\RelationController::class, 'saveQuestionRelations'])->name('save-question-relations');
-
-
-    Route::get('/banks', [App\Http\Controllers\Admin\BankController::class, 'index'])->name('banks');
-    Route::post('/save-bank', [App\Http\Controllers\Admin\BankController::class, 'saveBank'])->name('save-bank');
-    Route::get('/delete-bank/{id}', [App\Http\Controllers\Admin\BankController::class, 'deleteBank'])->name('del-bank');
-
-    Route::get('/roles', [App\Http\Controllers\Admin\RoleController::class, 'index'])->name('roles');
-    Route::post('/save-role', [App\Http\Controllers\Admin\RoleController::class, 'saveRole'])->name('save-role');
-    Route::get('/delete-role/{id}', [App\Http\Controllers\Admin\RoleController::class, 'deleteRole'])->name('del-role');
-
-    Route::get('/forms', [App\Http\Controllers\Admin\FormController::class, 'index'])->name('forms');
-    Route::post('/save-form', [App\Http\Controllers\Admin\FormController::class, 'saveForm'])->name('save-form');
-    Route::get('/delete-form/{id}', [App\Http\Controllers\Admin\FormController::class, 'deleteForm'])->name('del-form');
-
-    Route::get('/medicines', [App\Http\Controllers\Admin\MedicController::class, 'index'])->name('medicines');
-    Route::post('/save-medic', [App\Http\Controllers\Admin\MedicController::class, 'saveMedic'])->name('save-medic');
-    Route::get('/delete-medic/{id}', [App\Http\Controllers\Admin\MedicController::class, 'deleteMedic'])->name('del-medic');
-
-    Route::get('/dosage', [App\Http\Controllers\Admin\DoseController::class, 'index'])->name('dosage');
-    Route::post('/save-dose', [App\Http\Controllers\Admin\DoseController::class, 'saveDose'])->name('save-dose');
-    Route::get('/delete-dose/{id}', [App\Http\Controllers\Admin\DoseController::class, 'deleteDose'])->name('del-dose');
-
-    Route::get('/blood-investigation', [App\Http\Controllers\Admin\BloodInvController::class, 'index'])->name('blood-investigation');
-    Route::post('/save-blood-inv', [App\Http\Controllers\Admin\BloodInvController::class, 'saveBloodInv'])->name('save-blood-inv');
-    Route::get('/delete-blood-inv/{id}', [App\Http\Controllers\Admin\BloodInvController::class, 'deleteBloodInv'])->name('del-blood-inv');
-
-    Route::get('/x-rays', [App\Http\Controllers\Admin\XrayController::class, 'index'])->name('xrays');
-    Route::post('/save-xray', [App\Http\Controllers\Admin\XrayController::class, 'saveXray'])->name('save-xray');
-    Route::get('/delete-xray/{id}', [App\Http\Controllers\Admin\XrayController::class, 'deleteXray'])->name('del-xray');
-
-    Route::get('/ultrasounds', [App\Http\Controllers\Admin\UltrasoundController::class, 'index'])->name('uss');
-    Route::post('/save-ultrasound', [App\Http\Controllers\Admin\UltrasoundController::class, 'saveUltrasound'])->name('save-us');
-    Route::get('/delete-ultrasound/{id}', [App\Http\Controllers\Admin\UltrasoundController::class, 'deleteUltrasound'])->name('del-us');
-
-    Route::get('/ct-scans', [App\Http\Controllers\Admin\CtscanController::class, 'index'])->name('ctscans');
-    Route::post('/save-ctscan', [App\Http\Controllers\Admin\CtscanController::class, 'save'])->name('save-ctscan');
-    Route::get('/delete-ctscan/{id}', [App\Http\Controllers\Admin\CtscanController::class, 'delete'])->name('del-ctscan');
-});
 
 Route::get('/', [App\Http\Controllers\User\LoginController::class, 'showLoginForm'])->name('staff-login');
 Route::post('/staff/login',[App\Http\Controllers\User\LoginController::class, 'login'])->name('staff.login');
@@ -158,8 +77,6 @@ Route::prefix('user')->middleware(['auth','is_user'])->group(function () {
     Route::get('/data-collector/{id}/{patientId}', [DataCollectorController::class, 'showCollectorForm'])->name('data-collector');
     Route::post('/data-collector/submit/{form_id}', [DataCollectorController::class, 'submitAnswers'])->name('save-data-collector');
     Route::post('/upload-voice', [DataCollectorController::class, 'uploadVoice'])->name('upload-voice');
-
-
 
     Route::post('/ai/ask', [AIChatController::class, 'ask']);
 
